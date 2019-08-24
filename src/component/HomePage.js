cdc19f0affd508654d40571287c5165307e5ea17
Wuhang <yanwuhang@gmail.com>
2019-08-24 19:11:17 +0800

    add title and background pic, will update in future

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 
 const style = {
-  backgroundImage: 'url(https://c-ssl.duitang.com/uploads/item/201610/17/20161017162712_vPkxK.jpeg)',
-  height: '100%',
-  width: '100%',
-  backgroundRepeat: 'no-repeat',
-  display: 'block',
-  position: 'absolute'
+  back_pic: {
+    backgroundImage: 'url(https://c-ssl.duitang.com/uploads/item/201610/17/20161017162712_vPkxK.jpeg)',
+    height: '100%',
+    width: '100%',
+    backgroundRepeat: 'no-repeat',
+    display: 'block',
+    position: 'absolute'
+  },
+  title: {
+    textShadow: '5px 5px 20px red',
+    margin: '30px',
+    fontSize: '25px',
+    fontStyle: 'italic',
+  }
 }
 
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div style={style}>
-        {"welcome to my blog!"}
+      <div style={style.back_pic}>
+        <div style={style.title}>
+          闫某某的博客
+        </div>
       </div>
     )
   }
