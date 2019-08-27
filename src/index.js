cac1db97faee193f693d3be0f17a614cf787503d
Wuhang <yanwuhang@gmail.com>
2019-08-27 20:56:52 +0800

    add react-hot-loader, css-loader, style-loader, url-loader

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDom from 'react-dom';
 import HomePage from './component/HomePage';
 import { Provider } from 'react-redux';
 import store from './reducer';
+import { AppContainer } from 'react-hot-loader';
+
+if(module.hot) {
+  module.hot.accept();
+}
 
 ReactDom.render(
-  <Provider store={store}>
-    <HomePage />
-  </Provider>,
+  <AppContainer>
+    <Provider store={store}>
+      <HomePage />
+    </Provider>
+  </AppContainer>,
   document.getElementById('app')
 );
